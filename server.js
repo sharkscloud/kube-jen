180025b26bc343e92d0fb62ecfe0640e2f517856
sharkscloud <cloudsecbakhty@gmail.com>
2022-01-01 14:03:12 -0500

    Adam baby shark

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Adam is a little shark. He bites everyone!!!</h1> \n');
+  res.send('<h1 style="color:green;">Adam is a little shark. He bites everyone and he doesnt care!!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
